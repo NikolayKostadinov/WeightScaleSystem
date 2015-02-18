69298928e4cb6a1a8e2c4263c1fb262971523279
niknhattan <nikhattan@yahoo.com>
2015-02-18 12:55:44 +0200

    Added overload of extension method of "string" ToByteArea with Alignment and Lenght. Added Unit Tests for extension methods

--- WeightScale.ComunicationProtocol/Helpers/StringExtentions.cs
+++ WeightScale.ComunicationProtocol/Helpers/StringExtentions.cs
@@ -4,10 +4,27 @@
 // </copyright>
 // <author>Nikolay Kostadinov</author>
 //--------------------------------------------------------------------------------
+
 namespace WeightScale.ComunicationProtocol.Helpers
 {
     using System;
     using System.Linq;
+
+    /// <summary>
+    /// Alignment of result byte area
+    /// </summary>
+    public enum Alignment
+    {
+        /// <summary>
+        /// Left alignment
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// Right Alignment
+        /// </summary>
+        Right
+    }
 
     /// <summary>
     /// Provides extension methods to string class
@@ -16,7 +33,6 @@ namespace WeightScale.ComunicationProtocol.Helpers
     {
         public static byte[] ToByteArray(this string str)
         {
-            // empty. need to be removed after repository contributors test
             byte[] result = new byte[str.Length];
 
             for (int i = 0; i < str.Length; i++)
@@ -24,6 +40,34 @@ namespace WeightScale.ComunicationProtocol.Helpers
                 result[i] = Convert.ToByte(str[i]);
             }
 
+            return result;
+        }
+
+        public static byte[] ToByteArray(this string str, Alignment align, int lenght)
+        {
+            if (str.Length > lenght)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                    "The lenght of output byte array must be greater than or equal to lenght of string. Lengt of string: {0}, Lenght of array: {1} .", 
+                    str.Length, 
+                    lenght));
+            }
+
+            string alignedString = str;
+            switch (align)
+            {
+                case Alignment.Left:
+                    alignedString = alignedString.PadRight(lenght, ' ');
+                    break;
+                case Alignment.Right:
+                    alignedString = alignedString.PadLeft(lenght, ' ');
+                    break;
+                default:
+                    throw new ArgumentException("Invalid argument \"align\". Must be Alignment.Legt or Alignment.Right");
+            }
+
+            byte[] result = alignedString.ToByteArray();
             return result;
         }
     }
---
+++ WeightScale.ComunicationProtocolTests/StringExtentionsTests.cs
@@ -0,0 +1,86 @@
+﻿//---------------------------------------------------------------------------------
+// <copyright file="StringExtentionsTests.cs" company="Business Management Systems">
+//     Copyright (c) Business Management Systems. All rights reserved.
+// </copyright>
+// <author>Nikolay Kostadinov</author>
+//--------------------------------------------------------------------------------
+
+namespace WeightScale.ComunicationProtocolTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using WeightScale.ComunicationProtocol.Helpers;
+
+    [TestClass]
+    public class StringExtentionsTests
+    {
+        [TestMethod]
+        public void ToByteArray_Without_Parameters_Returns_Expected()
+        {
+            // Arrange
+            byte[] expected = new byte[] { 1, 2, 3 };
+            string input = new string(new char[]
+                {
+                    Convert.ToChar(1),
+                    Convert.ToChar(2),
+                    Convert.ToChar(3)
+                });
+
+            // Act
+            byte[] actual = input.ToByteArray();
+
+            // Assert
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void ToByteArray_With_Left_Parameter_Returns_Expected()
+        {
+            // Arrange
+            byte[] expected = new byte[] { 32, 32, 1, 2, 3 };
+            string input = new string(new char[]
+                {
+                    Convert.ToChar(1),
+                    Convert.ToChar(2),
+                    Convert.ToChar(3)
+                });
+
+            // Act
+            byte[] actual = input.ToByteArray(Alignment.Right, 5);
+
+            // Assert
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void ToByteArray_With_Right_Parameter_Returns_Expected()
+        {
+            // Arrange
+            byte[] expected = new byte[] { 1, 2, 3, 32, 32 };
+            string input = new string(new char[]
+                {
+                    Convert.ToChar(1),
+                    Convert.ToChar(2),
+                    Convert.ToChar(3)
+                });
+
+            // Act
+            byte[] actual = input.ToByteArray(Alignment.Left, 5);
+
+            // Assert
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
